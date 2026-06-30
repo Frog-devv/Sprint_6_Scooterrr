4f8e49e1ad18086fd594d1e8eabaf8190ce6a91f
Salta <salta@mail.ru>
2026-06-30 22:57:48 +0500

    add .idea

--- src/main/java/pageObjects/MainPage.java
+++ src/main/java/pageObjects/MainPage.java
@@ -7,7 +7,7 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainPage {
-    private WebDriver driver;
+    private final WebDriver driver;
 
     // Локаторы для заказа
     private final By cookieButton = By.id("rcc-confirm-button");
